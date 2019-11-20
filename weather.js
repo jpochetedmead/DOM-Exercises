22aa675a35753e0d4925699a2fe7ef2f862f8f2a
Julio Pochet Edmead <jpe3841@gmail.com>
2019-11-20 15:45:11 -0500

    Added all the files to start the exercises.

---
+++ weather.js
@@ -0,0 +1,24 @@
+const WEATHER_DATA = {
+  location: 'Lancaster, PA',
+  currentTemp: 50,
+  tempUnit: 'F',
+  status: 'Cloudy',
+  hourly: [
+    { time: '12:00 pm', temp: 49, precip: 0 },
+    { time: '1:00 pm', temp: 50, precip: 0 },
+    { time: '2:00 pm', temp: 52, precip: 0 },
+    { time: '3:00 pm', temp: 50, precip: 0 },
+    { time: '4:00 pm', temp: 49, precip: 0 },
+    { time: '5:00 pm', temp: 48, precip: 10 },
+  ],
+  daily: [
+    { date: '11/20', day: 'Wednesday', high: 50, low: 33, status: 'Partly Cloudy' },
+    { date: '11/21', day: 'Thursday', high: 52, low: 44, status: 'Partly Cloudy' },
+    { date: '11/22', day: 'Friday', high: 55, low: 28, status: 'AM Showers' },
+    { date: '11/23', day: 'Saturday', high: 47, low: 37, status: 'PM Showers' },
+  ]
+};
+
+
+// Your code should start after this line
+
